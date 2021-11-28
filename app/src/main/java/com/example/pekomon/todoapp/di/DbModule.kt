bea6bae4abd04985c28b14f6d0a03beeadce8ab2
Pekka Räsänen <5183385+pekomon@users.noreply.github.com>
2021-11-28 16:01:58 +0200

    Setup dagger-hilt

---
+++ app/src/main/java/com/example/pekomon/todoapp/di/DbModule.kt
@@ -0,0 +1,35 @@
+package com.example.pekomon.todoapp.di
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.pekomon.todoapp.data.db.TodoDataBase
+import com.example.pekomon.todoapp.util.Consts.DATABASE_NAME
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DbModule {
+
+    @Singleton
+    @Provides
+    fun provideDb(
+        @ApplicationContext context: Context
+    ) = Room.databaseBuilder(
+        context,
+        TodoDataBase::class.java,
+        DATABASE_NAME
+    )
+
+    @Singleton
+    @Provides
+    fun provideToDoDao(
+        db: TodoDataBase
+    ) = db.toDoDao()
+}
