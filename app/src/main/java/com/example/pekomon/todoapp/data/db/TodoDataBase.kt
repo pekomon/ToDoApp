522d1fcf93c930e799cfd8a3a821b91df4e8fc11
Pekka Räsänen <5183385+pekomon@users.noreply.github.com>
2021-12-02 22:10:53 +0200

    Fix entity class

--- app/src/main/java/com/example/pekomon/todoapp/data/db/TodoDataBase.kt
+++ app/src/main/java/com/example/pekomon/todoapp/data/db/TodoDataBase.kt
@@ -2,11 +2,12 @@ package com.example.pekomon.todoapp.data.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.example.pekomon.todoapp.data.models.ToDoTask
 
 private const val DB_VERSION = 1
 
 @Database(
-    entities = [ToDoDao::class],
+    entities = [ToDoTask::class],
     version = DB_VERSION,
     exportSchema = false
 )
