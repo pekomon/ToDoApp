b69f662b5aa13a6dfedb6ce1d579e5c29a025503
Pekka Räsänen <5183385+pekomon@users.noreply.github.com>
2021-12-10 22:30:16 +0200

    Add colors tor top bar

--- app/src/main/java/com/example/pekomon/todoapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/pekomon/todoapp/ui/theme/Color.kt
@@ -7,8 +7,14 @@ val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
 
+val lightGray = Color(0xFFFCFCFC)
+val mediumGray = Color(0xFF9C9C9C)
+val darkGray = Color(0xFF141414)
+
 // Priority colors
 val LowPriority = Color(0xFF00C980)
 val MediumPriority = Color(0xFFFFC114)
 val HighPriority = Color(0xFFFF4646)
 val NoPriority = Color(0xFFFFFFFF)
+
+
