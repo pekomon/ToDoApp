a70b5f3822853b70787e145121f7d617f2e358d2
Pekka Räsänen <5183385+pekomon@users.noreply.github.com>
2021-12-11 08:34:38 +0200

    Colors for fab

--- app/src/main/java/com/example/pekomon/todoapp/extensions/ColorsExtensions.kt
+++ app/src/main/java/com/example/pekomon/todoapp/extensions/ColorsExtensions.kt
@@ -4,6 +4,8 @@ import androidx.compose.material.Colors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import com.example.pekomon.todoapp.ui.theme.Purple500
+import com.example.pekomon.todoapp.ui.theme.Purple700
+import com.example.pekomon.todoapp.ui.theme.Teal200
 import com.example.pekomon.todoapp.ui.theme.lightGray
 
 val Colors.topAppBarColor: Color
@@ -13,3 +15,7 @@ val Colors.topAppBarColor: Color
 val Colors.topAppBarBackgroundColor: Color
     @Composable
     get() = if (isLight) Purple500 else Color.Black
+
+val Colors.fabBackgroundColor: Color
+    @Composable
+    get() = if (isLight) Teal200 else Purple700
--- app/src/main/java/com/example/pekomon/todoapp/ui/views/todolist/TodoListView.kt
+++ app/src/main/java/com/example/pekomon/todoapp/ui/views/todolist/TodoListView.kt
@@ -2,6 +2,7 @@ package com.example.pekomon.todoapp.ui.views.todolist
 
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -10,6 +11,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.pekomon.todoapp.R
+import com.example.pekomon.todoapp.extensions.fabBackgroundColor
 
 @Composable
 fun TodoListView(
@@ -30,7 +32,8 @@ fun ListFab(
     onClick: ((Int) -> Unit)
 ) {
     FloatingActionButton(
-        onClick = { onClick(-1) }
+        onClick = { onClick(-1) },
+        backgroundColor = MaterialTheme.colors.fabBackgroundColor
     ) {
         Icon(
             imageVector = Icons.Filled.Add,
