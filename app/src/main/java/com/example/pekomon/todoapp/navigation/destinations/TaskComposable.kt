0258380c8476620d16d4ab4af4f0a21fc728bca9
Pekka Räsänen <5183385+pekomon@users.noreply.github.com>
2022-03-14 22:33:53 +0200

    Update UI when task changes

--- app/src/main/java/com/example/pekomon/todoapp/navigation/destinations/TaskComposable.kt
+++ app/src/main/java/com/example/pekomon/todoapp/navigation/destinations/TaskComposable.kt
@@ -28,7 +28,7 @@ fun NavGraphBuilder.taskComposable(
         todoViewModel.getTask(taskId = taskId)
         val selectedTask by todoViewModel.selectedTask.collectAsState()
 
-        LaunchedEffect(key1 = taskId) {
+        LaunchedEffect(key1 = selectedTask) {
             todoViewModel.updateUI(task = selectedTask)
         }
 
